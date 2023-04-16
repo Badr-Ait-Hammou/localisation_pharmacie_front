9ae6dfc7b02ea60aabc292d992215c468d991595
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-04-16 19:05:16 +0000

    update garde

--- src/components/GardeTable.js
+++ src/components/GardeTable.js
@@ -1,76 +1,159 @@
 
 import axios from "axios";
 import React,{useState,useEffect} from "react";
-
-
-
-
-
-
-
-
+import Button from "@mui/material/Button";
+import Modal from "react-modal";
 
 export default function GardeTable() {
-    const [gardes, setgardes] = useState([]);
+    const [gardes, setGardes] = useState([]);
+    const [gardeType, setGardeType] = useState('');
+    const [selectedGarde, setSelectedGarde] = useState(null);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+
+
 
     useEffect(() => {
-        axios.get("http://localhost:8080/api/gardes/").then((response) => {
-            setgardes(response.data);
-        });
+        const getGarde = async () => {
+            const res = await fetch('http://localhost:8080/api/gardes/');
+            const getdata = await res.json();
+            setGardes(getdata);
+            loadGardes();
+        }
+        getGarde();
     }, []);
 
-    const handleDelete = (id) => {
-        if (window.confirm("Are you sure you want to delete this city?")) {
-            axios.delete(`http://localhost:8080/api/gardes/${id}`).then(() => {
-                setgardes(gardes.filter((garde) => garde.id !== id));
+
+
+    const loadGardes=async ()=>{
+        const res=await axios.get("http://localhost:8080/api/gardes/");
+        setGardes(res.data);
+    }
+
+    const handleDelete = (gardeId) => {
+        if (window.confirm("Are you sure you want to delete this Item?")) {
+            axios.delete(`http://localhost:8080/api/gardes/${gardeId}`).then(() => {
+                setGardes(gardes.filter((garde) => garde.id !== gardeId));
             });
         }
     };
 
-    const handleEdit = (id) => {
-        const newName = window.prompt("Enter the new name for this city:");
-        if (newName) {
-            axios.put(`http://localhost:8080/api/gardes/update/${id}`, {type:newName }).then(() => {
-                setgardes(gardes.map((garde) => {
-                    if (garde.id === id) {
-                        return { ...garde, type: newName };
-                    }
+
+    const handleOpenModal = (garde) => {
+        setSelectedGarde(garde);
+        setModalIsOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalIsOpen(false)
+    };
+
+
+
+    const handleEditVille = async (id) => {
+        try {
+            const response = await axios.put(`http://localhost:8080/api/gardes/${id}`, {
+                type: gardeType,
+
+            })
+            const updatedGardes = gardes.map((garde) => {
+                if (garde.id === id) {
+                    return response.data;
+                }else{
                     return garde;
-                }));
+                }
             });
+            setGardes(updatedGardes);
+            setModalIsOpen(false);
+            loadGardes();
+        } catch (error) {
+            console.error(error);
         }
     };
+
+
 
     return (
         <div>
+            <div className="table-responsive">
+                <table className="table mt-5 text-center">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Actions</th>
 
-            <table className="table mt-5 text-center">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>type</th>
-                    <th>actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {gardes.map((garde) => (
-                    <tr key={garde.id}>
-                        <td>{garde.id}</td>
-                        <td>{garde.type}</td>
-                        <td>
-                            <button className="btn btn-danger" onClick={() => handleDelete(garde.id)}>
-                                Delete
-                            </button>
-                            <button className="btn btn-secondary ml-2" onClick={() => handleEdit(garde.id)}>
-                                Edit
-                            </button>
-                        </td>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {gardes.map((garde,index)=>(
+                        <tr key={index}>
+                            <th scope="row">{garde.id}</th>
+                            <td>{garde.type}</td>
+                            <td>
 
+                                <Button variant="contained" color="info" onClick={() => handleOpenModal(garde)} >edit</Button>
+
+                                <Button variant="contained" color="warning" sx={{ ml:2 }}onClick={() => handleDelete(garde.id)}>delete</Button>
+
+                            </td>
+                        </tr>
+                    ))}
+
+                    </tbody>
+                </table>
+            </div>
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+
+                style={{
+                    overlay: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        zIndex: 1000
+                    },
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                        backgroundColor: '#fff',
+                        borderRadius: '10px',
+                        boxShadow: '20px 30px 25px rgba(0, 0, 0, 0.2)',
+                        padding: '20px',
+                        width:'350px',
+                        height:'300px'
+                    }
+                }}
+            >
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title" id="modal-modal-title">Update Garde</h5>
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="user-nom" className="form-label">Garde:</label>
+                                <input type="text" className="form-control" id="user-nom" value={gardeType} onChange={(e) => setGardeType(e.target.value)} />
+                            </div>
+
+                        </form>
+                        <div className="d-flex justify-content-center mt-3">
+                            <Button variant="contained" color="error" onClick={handleCloseModal}>
+                                Annuler
+                            </Button>
+                            <Button variant="contained" color="success" sx={{ ml:1 }} onClick={() => handleEditVille(selectedGarde.id)}>
+                                Sauvegarder
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
+
 
 }
